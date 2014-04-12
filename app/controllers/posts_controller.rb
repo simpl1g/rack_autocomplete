41eeba7f5f3277d306aba84fa0230835ce77c06f
Konstantin Ilchenko <konstantin@ilchenko.by>
2014-04-12 22:44:42 +0300

    add controller autocomplete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,6 +61,11 @@ class PostsController < ApplicationController
     end
   end
 
+  def autocomplete
+    posts = Post.where("title LIKE ?", "#{params[:q]}_%").limit(10).pluck(:title)
+    render json: posts.to_json
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
--- app/middleware/posts_autocomplete.rb
+++ app/middleware/posts_autocomplete.rb
@@ -6,9 +6,8 @@ class PostsAutocomplete
   def call(env)
     if env['REQUEST_PATH'] == '/posts_autocomplete'
       request = Rack::Request.new(env)
-      term = request.params['term']
       posts = ActiveRecord::Base.connection_pool.with_connection do
-        Post.where("title LIKE ?", "#{term}_%").limit(10).pluck(:title)
+        Post.where("title LIKE ?", "#{request.params['q']}_%").limit(10).pluck(:title)
       end
       [200, {'Content-Type' => 'application/json'}, [posts.to_json]]
     else
